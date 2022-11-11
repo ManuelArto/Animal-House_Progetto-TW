5a88ab1e160ab83cf3e9ac6e62b22db81cca9854
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2022-11-11 20:47:35 +0100

    Fix home router-link

--- Game-AnimalHouse/src/views/Home.vue
+++ Game-AnimalHouse/src/views/Home.vue
@@ -39,81 +39,74 @@ let services = [{
 </script>
 
 <template>
-    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <div id="hero-1">
-            <div class="overlay-1">
-              <h1><b>I migliori prodotti <br>li trovi da noi</b></h1>
-              <h6>Cerca nel nostro store i prodotti <br>più adatti al tuo amico</h6>
-            </div>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <div id="hero-2">
-            <div class="overlay-2">
-              <h1><b>Scopri i nostri servizi</b></h1>
-              <h6>Villegiature, psicologi, pet sitter. <br>Tutti i servizi a portata di un click .</h6>
-            </div>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <div id="hero-3">
-            <div class="overlay-3">
-              <h1><b>Le informazioni di <br>cui hai bisogno</b></h1>
-              <h6>Villegiature, psicologi, pet sitter. <br>Leggi tutte le informazioni</h6>
-            </div>
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div id="hero-1">
+          <div class="overlay-1">
+            <h1><b>I migliori prodotti <br>li trovi da noi</b></h1>
+            <h6>Cerca nel nostro store i prodotti <br>più adatti al tuo amico</h6>
           </div>
         </div>
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+      <div class="carousel-item">
+        <div id="hero-2">
+          <div class="overlay-2">
+            <h1><b>Scopri i nostri servizi</b></h1>
+            <h6>Villegiature, psicologi, pet sitter. <br>Tutti i servizi a portata di un click .</h6>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div id="hero-3">
+          <div class="overlay-3">
+            <h1><b>Le informazioni di <br>cui hai bisogno</b></h1>
+            <h6>Villegiature, psicologi, pet sitter. <br>Leggi tutte le informazioni</h6>
+          </div>
+        </div>
+      </div>
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 
-    <div class="container px-3">
-      <div class="common_heading d-flex py-3">
-        <h2 class="fw-bold m-auto">Tutti i servizi per te e il tuo pet</h2>
-      </div>
-      <b-row class="g-3 mb-5">
-        <div v-for="service in services" class="col-md-4 col-12">
-          <div class="boxes_margin p-4 h-100 d-flex flex-column text-center">
-            <i :class="service.icon"></i>
-            <div class="common_heading">
-              <h4>{{ service.name }}</h4>
-            </div>
-            <p>
-              {{ service.description }}
-            </p>
-            <button class="btn btn-primary btn-lg mt-auto mx-auto" :to="service.path">Scopri di più</button>
-          </div>
-        </div>
-      </b-row>
+  <div class="container px-3">
+    <div class="common_heading d-flex py-3">
+      <h2 class="fw-bold m-auto">Tutti i servizi per te e il tuo pet</h2>
     </div>
+    <b-row class="g-3 mb-5">
+      <div v-for="service in services" class="col-md-4 col-12">
+        <div class="boxes_shadow p-4 h-100 d-flex flex-column text-center">
+          <i :class="service.icon"></i>
+          <div class="common_heading">
+            <h4>{{ service.name }}</h4>
+          </div>
+          <p>
+            {{ service.description }}
+          </p>
+          <router-link tag="button" class="btn btn-primary btn-lg boxes_shadow mt-auto mx-auto" :to="service.path">
+            Scopri di più</router-link>
+        </div>
+      </div>
+    </b-row>
+  </div>
 </template>
 
 <style scoped lang="scss">
-
 .container {
   width: 70%;
 }
 
-.boxes_margin{
+.boxes_shadow {
   box-shadow: 2px 2px 20px rgb(0 0 0 / 10%);
   border-radius: 10px;
-  
-  &:hover {
-    box-shadow: 2px 2px 60px rgba(25, 66, 189, 0.862);
-  }
-}
-.btn {
-  box-shadow: 2px 2px 20px rgb(0 0 0 / 10%);
-  border-radius: 10px;
+
   &:hover {
     box-shadow: 2px 2px 60px rgba(25, 66, 189, 0.862);
   }
@@ -127,6 +120,8 @@ let services = [{
 .common_heading {
   color: #2f4eec;
 }
+
+// CAROUSEL
 
 .carousel-inner {
   width: 100%;
