c7d6f70011c78ab2e410c188e0be9a957d585eec
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2022-11-18 00:03:08 +0100

    VideosView refactor

--- Game-AnimalHouse/src/views/VideosView.vue
+++ Game-AnimalHouse/src/views/VideosView.vue
@@ -2,18 +2,18 @@
 let changedLike = false;
 let changedDisLike = false;
 const videos = [
-    { src: "https://www.youtube.com/embed/bTcXkLlGFeQ?controls=0", title: "Compilation", description: "Serie di video in cui i nostri animaletti ci fanno morire dal ridere durante le loro azioni quotidiane", index: 1 },
-    { src: "https://www.youtube.com/embed/OaQePr10CuA?start=8&controls=0", title: "Compilation", description: "Serie di video in cui i nostri animaletti ci fanno morire dal ridere durante le loro azioni quotidiane", index: 2 },
-    { src: "https://www.youtube.com/embed/a4P61RKrkJc?start=15&controls=0", title: "Animali o supereroi?", description: "Possiamo ammirare il coraggio che hanno questi animali nel salvare delle persone.", index: 3 },
-    { src: "https://www.youtube.com/embed/B6tGDtwh85U?start=15&controls=0", title: "Cani e bimbi", description: "Possiamo osservare 10 razze di cane che vanno molto d'accordo con i nostri bimbi.", index: 4 },
-    { src: "https://www.youtube.com/embed/uDzFpp715_0?start=15&controls=0", title: "Cucciolotti", description: "Ecco una compilation di cuccioli meravigliosi", index: 5 },
-    { src: "https://www.youtube.com/embed/d4j8ibsjpBM?controls=0", title: "Cani o nani", description: "In questo video osserveremo le 10 razze di cane più piccole.", index: 6 },
-    { src: "https://www.youtube.com/embed/OYQllOzeT3s&controls=0", title: "Animali vs Specchio", description: "Possiamo osservare la reazione dei nostri amici davanti ad uno specchio", index: 7 },
-    { src: "https://www.youtube.com/embed/AwctIwpiz_E&controls=0", title: "Animali enormi", description: "Troveremo gli esemplari di animali più grande del pianeta", index: 8 },
-    { src: "https://www.youtube.com/embed/ewbARWUttFw&controls=0", title: "Amore e odio", description: "Sappiamo tutti che tra cane e gatto c'è un rapporto speciale, fatto di amore ma anche di odio", index: 9 },
-    { src: "https://www.youtube.com/embed/Znd3AJI2kt0&controls=0", title: "Animali o calciatori", description: "Possiamo vedere alcune scene buffe in cui gli animali chiedono autografi ai calciatori", index: 10 },
-    { src: "https://www.youtube.com/embed/JhaXTd8DKJk=cotrols=0", title: "I cani e l'acqua", description: "Sappiamo che ci sono molti cani che adorano l'acqua, ma anche tanti che la odiano. Osserviamone alcuni.", index: 11 },
-    { src: "https://www.youtube.com/embed/YbAU0YI4kV4&controls=0", title: "Expo 2016", description: "Possiamo osservare i nostri amici a quattro zampe all'Expo che si è svolto a Milano nel 2016", index: 12 }
+    { src: "https://www.youtube.com/embed/bTcXkLlGFeQ", title: "Compilation", description: "Serie di video in cui i nostri animaletti ci fanno morire dal ridere durante le loro azioni quotidiane"},
+    { src: "https://www.youtube.com/embed/a4P61RKrkJc?start=15", title: "Animali o supereroi?", description: "Possiamo ammirare il coraggio che hanno questi animali nel salvare delle persone."},
+    { src: "https://www.youtube.com/embed/B6tGDtwh85U?start=15", title: "Cani e bimbi", description: "Possiamo osservare 10 razze di cane che vanno molto d'accordo con i nostri bimbi."},
+    { src: "https://www.youtube.com/embed/uDzFpp715_0?start=15", title: "Cucciolotti", description: "Ecco una compilation di cuccioli meravigliosi"},
+    { src: "https://www.youtube.com/embed/d4j8ibsjpBM", title: "Cani o nani", description: "In questo video osserveremo le 10 razze di cane più piccole."},
+    { src: "https://www.youtube.com/embed/OYQllOzeT3s", title: "Animali vs Specchio", description: "Possiamo osservare la reazione dei nostri amici davanti ad uno specchio"},
+    { src: "https://www.youtube.com/embed/OaQePr10CuA?start=8", title: "Compilation", description: "Serie di video in cui i nostri animaletti ci fanno morire dal ridere durante le loro azioni quotidiane"},
+    { src: "https://www.youtube.com/embed/AwctIwpiz_E", title: "Animali enormi", description: "Troveremo gli esemplari di animali più grande del pianeta"},
+    { src: "https://www.youtube.com/embed/ewbARWUttFw", title: "Amore e odio", description: "Sappiamo tutti che tra cane e gatto c'è un rapporto speciale, fatto di amore ma anche di odio"},
+    { src: "https://www.youtube.com/embed/Znd3AJI2kt0", title: "Animali o calciatori", description: "Possiamo vedere alcune scene buffe in cui gli animali chiedono autografi ai calciatori"},
+    { src: "https://www.youtube.com/embed/JhaXTd8DKJk=cotrols=0", title: "I cani e l'acqua", description: "Sappiamo che ci sono molti cani che adorano l'acqua, ma anche tanti che la odiano. Osserviamone alcuni."},
+    { src: "https://www.youtube.com/embed/YbAU0YI4kV4", title: "Expo 2016", description: "Possiamo osservare i nostri amici a quattro zampe all'Expo che si è svolto a Milano nel 2016"}
 ]   
 function myLike(x) {
     if (!changedLike && !changedDisLike) {
@@ -51,50 +51,42 @@ function share(x){
 </script>
 
 <template>
-    <b-container>
-        <h1 id="title" class="fw-bold m-auto text-center">Video buffi</h1>
-        <div id="body">
-            <b-row>
-                <b-col v-for="video in videos">
-                    <div class="animal-video">
-                        <b-row>
-                            <div class="col-12">
-                                <div class="animal-video-title mb-2"><strong> {{ video.title }} </strong></div>
-                            </div>
-                        </b-row>
-                        <b-row>
-                            <div class="vid">
-                                <iframe :src="video.src" style="width:100%; height:250px;border-radius: 10px;" allowfullscreen></iframe>
-                            </div>
-                        </b-row>
-                        <b-row>
-                            <b-col>
-                                <b-button id="btn" @click="myLike(video.index)">
-                                    <i :id="'like' + video.index" class="bi bi-hand-thumbs-up-fill"></i>
-                                </b-button>
-                            </b-col>
-                            <b-col>
-                                <b-button id="btn" @click="myDisLike(video.index)">
-                                    <i :id="'dislike' + video.index" class="bi bi-hand-thumbs-down-fill"></i>
-                                </b-button>
-                            </b-col>
-                            <b-col>
-                                <textarea :id="'link' + video.index" hidden>{{video.src}}</textarea>
-                                <b-button id="btn" @click="share(video.index)" data-bs-toggle="modal" data-bs-target="#notify">
-                                    <i id="share" class="bi bi-share-fill"></i>
-                                </b-button>
-                            </b-col>
-                        </b-row>
-                        <b-row>
-                            <div class="col-12">
-                                <div class="text_18"> {{ video.description }} </div>
-                            </div>
-                        </b-row>
+    <b-container id="animation" class="position-static">
+        <h1 id="title" class="fw-bold p-3 m-auto text-center">Video buffi</h1>
+        <b-row>
+            <b-col class="d-flex justify-content-center" v-for="(video, index) in videos">
+                <div class="animal-video p-2" id="animation">
+                    <div class="animal-video-title mb-2"><strong> {{ video.title }} </strong></div>
+                    <div class="vid">
+                        <iframe :src="video.src" style="width:100%; height:250px;border-radius: 10px;" allowfullscreen></iframe>
                     </div>
-                    <br>
-                </b-col>
-            </b-row>
-        </div>
+                    <b-row class="d-flex justify-content-between text-center">
+                        <b-col>
+                            <b-button id="btn" @click="myLike(index)">
+                                <i :id="'like' + index" class="bi bi-hand-thumbs-up-fill"></i>
+                            </b-button>
+                        </b-col>
+                        <b-col>
+                            <b-button id="btn" @click="myDisLike(index)">
+                                <i :id="'dislike' + index" class="bi bi-hand-thumbs-down-fill"></i>
+                            </b-button>
+                        </b-col>
+                        <b-col>
+                            <textarea :id="'link' + index" hidden>{{video.src}}</textarea>
+                            <b-button id="btn" @click="share(index)" data-bs-toggle="modal" data-bs-target="#notify">
+                                <i id="share" class="bi bi-share-fill"></i>
+                            </b-button>
+                        </b-col>
+                    </b-row>
+                    <b-row>
+                        <div class="col-12">
+                            <div class="text_18"> {{ video.description }} </div>
+                        </div>
+                    </b-row>
+                </div>
+                <br>
+            </b-col>
+        </b-row>
     </b-container>
 
     <div class="modal fade" id="notify" tabindex="-1" role="dialog" aria-labelledby="notify" aria-hidden="true">
@@ -114,20 +106,21 @@ function share(x){
 <style scoped>
 #title {
     color: #2f4eec;
-    animation: zoom 0.5s ease-in both;
+    animation: fadeIn 0.5s ease-in both;
 }
 
-#body {
-    animation: zoom 0.5s ease-in both;
-    
+#animation {
+    animation: fadeIn 0.5s ease-in both;
 }
 
-@keyframes zoom {
+@keyframes fadeIn {
 	from {
-		transform: scale(1.5);
+		opacity: 0;
+		transform: translate3d(0, -20%, 0);
 	}
 	to {
-		transform: scale(1);
+		opacity: 1;
+		transform: translate3d(0, 0, 0);
 	}
 }
 
@@ -153,10 +146,12 @@ function share(x){
     display:block;
 }
 
-.vid :hover{
-    transition:all 0.4s ease 0s;
-    transform:scale(1.3);
-    z-index:2;
+@media screen and (min-width: 564px) {
+    .vid :hover{
+        transition:all 0.4s ease 0s;
+        transform:scale(1.3);
+        z-index:2;
+    }
 }
 .animal-video{
     background: white;
