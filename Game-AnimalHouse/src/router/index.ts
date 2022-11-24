8206d9a0675e359496633e5f645d3a0c757384f4
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2022-11-24 17:20:29 +0100

    Fix router

--- Game-AnimalHouse/src/router/index.ts
+++ Game-AnimalHouse/src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import { state } from '@/store'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
