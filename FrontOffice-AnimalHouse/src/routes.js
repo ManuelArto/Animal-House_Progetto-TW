31238699f7ba07ea1af2529157e223f1e385f637
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2022-12-03 12:11:51 +0100

    Dynamically import spa

--- FrontOffice-AnimalHouse/src/routes.js
+++ FrontOffice-AnimalHouse/src/routes.js
@@ -1,30 +1,39 @@
+import { wrap } from 'svelte-spa-router/wrap'
+
 import Home from "./lib/views/Home.svelte";
-import Profile from "./lib/views/Profile.svelte"
-import Shop from "./lib/views/Shop.svelte"
-import ServiziPresenza from "./lib/views/ServiziPresenza.svelte"
-// Servizi di Comunità
-import Leaderboard from "./lib/views/ServiziComunità/Leaderboard.svelte";
-import BachecaEccoloQua from "./lib/views/ServiziComunità/BachecaEccoloQua.svelte";
-import BachecaCercoPartner from "./lib/views/ServiziComunità/BachecaCercoPartner.svelte";
-import BachecaAiutami from "./lib/views/ServiziComunità/BachecaAiutami.svelte";
-// Servizi Online
-import VideoConfEsperto from "./lib/views/ServiziOnline/VideoConfEsperto.svelte";
-import VideoConfVeterinaio from "./lib/views/ServiziOnline/VideoConfVeterinaio.svelte";
 import NotFound from "./lib/views/NotFound.svelte";
 
 
 export const routes = {
-  "/" : Home,
-  "/profile" : Profile,
-  "/shop" : Shop,
-  "/serviziPresenza" : ServiziPresenza,
+  "/": Home,
+  "/profile": wrap({
+    asyncComponent: () => import('./lib/views/Profile.svelte')
+  }),
+  "/shop": wrap({
+    asyncComponent: () => import('./lib/views/Shop.svelte')
+  }),
+  "/serviziPresenza": wrap({
+    asyncComponent: () => import('./lib/views/ServiziPresenza.svelte')
+  }),
   // Servizi di Comunità
-  "/servizi/leaderboard" : Leaderboard,
-  "/servizi/bachecaEccoloQua" : BachecaEccoloQua,
-  "/servizi/bachecaCercoPartner" : BachecaCercoPartner,
-  "/servizi/bachecaAiutami" : BachecaAiutami,
+  "/servizi/leaderboard": wrap({
+    asyncComponent: () => import('./lib/views/ServiziComunità/Leaderboard.svelte')
+  }),
+  "/servizi/bachecaEccoloQua": wrap({
+    asyncComponent: () => import('./lib/views/ServiziComunità/BachecaEccoloQua.svelte')
+  }),
+  "/servizi/bachecaCercoPartner": wrap({
+    asyncComponent: () => import('./lib/views/ServiziComunità/BachecaCercoPartner.svelte')
+  }),
+  "/servizi/bachecaAiutami": wrap({
+    asyncComponent: () => import('./lib/views/ServiziComunità/BachecaAiutami.svelte')
+  }),
   // Servizi Online
-  "/serviziOnline/videoConfEsperto" : VideoConfEsperto,
-  "/serviziOnline/videoConfVeterinaio" : VideoConfVeterinaio,
-  "*" : NotFound,
+  "/serviziOnline/videoConfEsperto": wrap({
+    asyncComponent: () => import('./lib/views/ServiziOnline/VideoConfEsperto.svelte')
+  }),
+  "/serviziOnline/videoConfVeterinaio": wrap({
+    asyncComponent: () => import('./lib/views/ServiziOnline/VideoConfVeterinaio.svelte')
+  }),
+  "*": NotFound,
 };
