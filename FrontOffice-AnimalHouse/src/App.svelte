05b217c2b70fd6dc828872301669e85372b78fd7
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2022-12-03 12:17:37 +0100

    add navbar

--- FrontOffice-AnimalHouse/src/App.svelte
+++ FrontOffice-AnimalHouse/src/App.svelte
@@ -1,12 +1,33 @@
 <script>
   import Router, { link } from "svelte-spa-router";
   import { routes } from "./routes.js";
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte'
 </script>
 
-<main>
-  <h2>NavBar</h2>
-  <Router {routes}/>
-</main>
+<Router {routes}/>
+    <Navbar>
+        <NavBrand href="/">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            class="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+          />
+          <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Flowbite
+          </span>
+        </NavBrand>
+        <div class="flex md:order-2">
+            <Button size="sm">Accedi</Button>
+            <Button class="ml-1" size="sm">Registrati</Button>
+        </div>        
+        <NavUl class="order-1">
+          <NavLi href="/" active={true}>Home</NavLi>
+          <NavLi href="/servizi/bachecaCercoPartner">About</NavLi>    
+          <NavLi href="/services">Services</NavLi>
+          <NavLi href="/pricing">Pricing</NavLi>
+          <NavLi href="/contact">Contact</NavLi>
+        </NavUl>
+    </Navbar>
 
 <style>
 </style>
