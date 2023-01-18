3ace18bbf8e86cec279ab34f614e7a2a68b1fd80
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-01-18 20:40:32 +0100

    Refactor src folder and add favicon

--- FrontOffice-AnimalHouse/src/App.svelte
+++ FrontOffice-AnimalHouse/src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Router, { link } from "svelte-spa-router";
+  import Router from "svelte-spa-router";
   import { routes } from "./routes.js";
   import Navbar from "./components/Navbar.svelte"
 </script>
--- FrontOffice-AnimalHouse/src/routes.js
+++ FrontOffice-AnimalHouse/src/routes.js
@@ -1,39 +1,39 @@
 import { wrap } from 'svelte-spa-router/wrap'
 
-import Home from "./lib/views/Home.svelte";
-import NotFound from "./lib/views/NotFound.svelte";
+import Home from "./views/Home.svelte";
+import NotFound from "./views/NotFound.svelte";
 
 
 export const routes = {
   "/": Home,
   "/profile": wrap({
-    asyncComponent: () => import('./lib/views/Profile.svelte')
+    asyncComponent: () => import('./views/Profile.svelte')
   }),
   "/shop": wrap({
-    asyncComponent: () => import('./lib/views/Shop.svelte')
+    asyncComponent: () => import('./views/Shop.svelte')
   }),
   "/serviziPresenza": wrap({
-    asyncComponent: () => import('./lib/views/ServiziPresenza.svelte')
+    asyncComponent: () => import('./views/ServiziPresenza.svelte')
   }),
   // Servizi di Comunità
   "/servizi/leaderboard": wrap({
-    asyncComponent: () => import('./lib/views/ServiziComunità/Leaderboard.svelte')
+    asyncComponent: () => import('./views/ServiziComunità/Leaderboard.svelte')
   }),
   "/servizi/bachecaEccoloQua": wrap({
-    asyncComponent: () => import('./lib/views/ServiziComunità/BachecaEccoloQua.svelte')
+    asyncComponent: () => import('./views/ServiziComunità/BachecaEccoloQua.svelte')
   }),
   "/servizi/bachecaCercoPartner": wrap({
-    asyncComponent: () => import('./lib/views/ServiziComunità/BachecaCercoPartner.svelte')
+    asyncComponent: () => import('./views/ServiziComunità/BachecaCercoPartner.svelte')
   }),
   "/servizi/bachecaAiutami": wrap({
-    asyncComponent: () => import('./lib/views/ServiziComunità/BachecaAiutami.svelte')
+    asyncComponent: () => import('./views/ServiziComunità/BachecaAiutami.svelte')
   }),
   // Servizi Online
   "/serviziOnline/videoConfEsperto": wrap({
-    asyncComponent: () => import('./lib/views/ServiziOnline/VideoConfEsperto.svelte')
+    asyncComponent: () => import('./views/ServiziOnline/VideoConfEsperto.svelte')
   }),
   "/serviziOnline/videoConfVeterinaio": wrap({
-    asyncComponent: () => import('./lib/views/ServiziOnline/VideoConfVeterinaio.svelte')
+    asyncComponent: () => import('./views/ServiziOnline/VideoConfVeterinaio.svelte')
   }),
   "*": NotFound,
 };
