1b7e8bb1f213301e41dcf2f8330af0afb62e8c89
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-01-24 18:44:29 +0100

    home con bug

--- FrontOffice-AnimalHouse/src/App.svelte
+++ FrontOffice-AnimalHouse/src/App.svelte
@@ -2,9 +2,14 @@
   import Router from "svelte-spa-router";
   import { routes } from "./routes.js";
   import Navbar from "./components/Navbar.svelte"
+  import Footer from "./components/Footer.svelte"
 </script>
 
 <Navbar />
 <Router {routes}/>
+
+  <Footer />
+
+	
 <style>
 </style>
