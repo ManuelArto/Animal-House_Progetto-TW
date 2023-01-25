25252e48d2bde8cfab9f234b78f43534a6a9cf36
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-01-25 10:51:05 +0100

    modify carousel (con bug)

--- FrontOffice-AnimalHouse/src/components/HomeCarousel.svelte
+++ FrontOffice-AnimalHouse/src/components/HomeCarousel.svelte
@@ -3,6 +3,6 @@
     import { CarouselTransition } from 'flowbite-svelte'
 </script>
 
-<div class="max-w-4xl">
+<div class="w-full">
     <CarouselTransition {images} transitionType="fade" transitionParams="{{ duration: 1000 }}" showCaptions={false} showThumbs={false} duration={5000} />
 </div>
