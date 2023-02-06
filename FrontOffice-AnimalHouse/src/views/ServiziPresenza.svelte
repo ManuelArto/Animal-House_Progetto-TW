42224f6282a07326744f28722d00d30a91932f77
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-02-06 17:05:56 +0100

    Aggiunti controlli animali e login

--- FrontOffice-AnimalHouse/src/views/ServiziPresenza.svelte
+++ FrontOffice-AnimalHouse/src/views/ServiziPresenza.svelte
@@ -1,8 +1,11 @@
 <script>
 	import { AccordionItem, Accordion, Modal, TableBody, TableBodyCell, TableBodyRow, Checkbox, Input, Label, Radio, TableHead, TableHeadCell, TableSearch, Card, Button } from "flowbite-svelte";
 	import { animals } from '../store'
+	import { user } from '../store'
 	import CardServices from "../components/CardServices.svelte";
 	let searchTerm = '';
+	let isUserLogged;
+	user.isUserLogged.subscribe(value => isUserLogged = value );
 	let data = [
 		{
 			"address": {
@@ -97,29 +100,17 @@
 	$: filteredItems = data.filter(
         (item) => item.address.street.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 
     );
-
+	
 	let error;
 	let date_insert = false;
 	let error_insert_service=false;
-	
+	let error_animal;
 	let selectedOption = $animals.name;
 
 	let total_services = ["Dogsitter", "Veterinario"];
 	let selected_sits = [];
 	let selected_service = [];
 	let date_value;
-
-	window.addEventListener("load", () => {
-		const dateInput = document.getElementById("input-date");
-		dateInput.addEventListener("change", function() {
-			if (!dateInput.value) {
-				date_insert = false;
-			} else {
-				date_insert = true;
-			}
-		});
-		
-	});
 	
 	function control(){
 		let form = document.getElementById("checkbox_services");
@@ -136,8 +127,13 @@
 		}else{
 			error = false;
 			error_insert_service = false;
-			document.getElementById("selezione").style.display = "none";
-			document.getElementById("prenotazione").style.display = "block";
+			if($animals.length == 0){
+				error_animal = true;
+			}else{
+				error_animal = false;
+				document.getElementById("selezione").style.display = "none";
+				document.getElementById("prenotazione").style.display = "block";
+			}
 		}
 
 		//TODO: riempimento valori checkbox con bug (devo prima far uscire un alert per poter salvare il contenuto)
@@ -183,74 +179,85 @@
 <style>
 </style>
 
-<p class="mt-10 ml-10 font-bold font-serif xl:text-4xl sm:text-lg text-gray-900 dark:text-white">Prenota il servizio desiderato dove vuoi e quando vuoi</p>
-
-
-<div id="selezione" style="display:block;"> 
-	<p class="mt-5 ml-10 font-serif xl:text-2xl sm:text-lg text-gray-900 dark:text-white">Compila i campi di seguito e controlla la nostra disponibilità</p>
-	<div class="xl:inline-flex sm:block mt-10 ml-5">
-		<Card class="xl:ml-5 xl:mb-64">
-			<div id="checkbox_services">
-				{#if date_insert}
-						<p class="text-lg text-red-600">Scegli il servizio desiderato (required)</p>
-				{:else}
-					<p class="text-lg text-dark">Scegli il servizio desiderato</p>
-				{/if}
-				<Checkbox class="mt-5 text-lg" name="example" value="Dogsitter">Dogsitter</Checkbox>
-				<Checkbox class="mt-5 text-lg" name="example" value="Veterinario">Veterinario</Checkbox>
-			</div>
-		</Card>
-		<Card class="xl:ml-5 max-h-96">
-			<p class="text-lg text-dark">Scegli la sede più comoda</p>
-			<TableSearch placeholder="Search by address" hoverable={true} bind:inputValue={searchTerm}>
-				<TableBody class="divide-y" id="checkbox_sits">
-					{#each filteredItems as item}
-						<TableBodyRow class="border-separate border-spacing-2 border border-slate-400">
-							<TableBodyCell><Checkbox value={item.address.street}>{item.address.street + ", " + item.address.city}</Checkbox></TableBodyCell>
-						</TableBodyRow>
-					{/each}
-				</TableBody>
-			</TableSearch>
-		</Card>
-
-		<Card class="xl:ml-5 xl:mb-72">
-			<p class="text-lg text-dark">Scegli la data desiderata *</p>
-			<div class="relative max-w-sm mt-5">
-				<input class="w-75 rounded-lg" type="date" id="input-date" bind:value={date_value}>
-			</div>
-		</Card>
+{#if !isUserLogged}
+	<div class="text-danger font-bold text-red-600 text-2xl mt-10 ml-24">
+		Per accedere a questa sezione devi eseguire l'accesso sulla piattaforma
 	</div>
+{:else}
+	<p class="mt-10 ml-10 font-bold font-serif xl:text-4xl sm:text-lg text-gray-900 dark:text-white">Prenota il servizio desiderato dove vuoi e quando vuoi</p>
+	<div id="selezione" style="display:block;"> 
+		<p class="mt-5 ml-10 font-serif xl:text-2xl sm:text-lg text-gray-900 dark:text-white">Compila i campi di seguito e controlla la nostra disponibilità</p>
+		<div class="xl:inline-flex sm:block mt-10 ml-5">
+			<Card class="xl:ml-5 xl:mb-64">
+				<div id="checkbox_services">
+					{#if date_insert}
+							<p class="text-lg text-red-600">Scegli il servizio desiderato (required)</p>
+					{:else}
+						<p class="text-lg text-dark">Scegli il servizio desiderato</p>
+					{/if}
+					<Checkbox class="mt-5 text-lg" name="example" value="Dogsitter">Dogsitter</Checkbox>
+					<Checkbox class="mt-5 text-lg" name="example" value="Veterinario">Veterinario</Checkbox>
+				</div>
+			</Card>
+			<Card class="xl:ml-5 max-h-96">
+				<p class="text-lg text-dark">Scegli la sede più comoda</p>
+				<TableSearch placeholder="Search by address" hoverable={true} bind:inputValue={searchTerm}>
+					<TableBody class="divide-y" id="checkbox_sits">
+						{#each filteredItems as item}
+							<TableBodyRow class="border-separate border-spacing-2 border border-slate-400">
+								<TableBodyCell><Checkbox value={item.address.street}>{item.address.street + ", " + item.address.city}</Checkbox></TableBodyCell>
+							</TableBodyRow>
+						{/each}
+					</TableBody>
+				</TableSearch>
+			</Card>
 
-	<div class="flex flex-row-reverse mr-20 mb-10">
-		<Button type="submit" on:click={control}>Prenota</Button>
+			<Card class="xl:ml-5 xl:mb-72">
+				<p class="text-lg text-dark">Scegli la data desiderata *</p>
+				<div class="relative max-w-sm mt-5">
+					<input class="w-75 rounded-lg" type="date" id="input-date" bind:value={date_value} on:change={() => date_insert = !date_insert}>
+				</div>
+			</Card>
+		</div>
+
+		<div class="flex flex-row-reverse mr-20 mb-10">
+			<Button type="submit" on:click={control}>Prenota</Button>
+		</div>
+		<div class="ml-10 mb-5">
+			<p class="text-xs">* Se inserisci la data, obbligatoriamente devi inserire il servizio</p>
+		</div>
 	</div>
-	<div class="ml-10 mb-5">
-		<p class="text-xs">* Se inserisci la data, obbligatoriamente devi inserire il servizio</p>
-	</div>
-	
-</div>
+{/if}	
 
 {#if error}
-<Modal bind:open={error} size="xs" autoclose>
-  <div class="text-center">
-      <svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-      <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Devi selezionare almeno una checkbox</h3>
-      <Button color='alternative'>ok</Button>
-  </div>
-</Modal>
+	<Modal bind:open={error} size="xs" autoclose>
+		<div class="text-center">
+			<svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+			<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Devi selezionare almeno una checkbox</h3>
+			<Button color='alternative'>ok</Button>
+		</div>
+	</Modal>
 {/if}
 
 {#if error_insert_service}
-<Modal bind:open={error} size="xs" autoclose>
-  <div class="text-center">
-      <svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-      <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Devi selezionare almeno un servizio se imposti una data</h3>
-      <Button color='alternative'>ok</Button>
-  </div>
-</Modal>
+	<Modal bind:open={error} size="xs" autoclose>
+		<div class="text-center">
+			<svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+			<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Devi selezionare almeno un servizio se imposti una data</h3>
+			<Button color='alternative'>ok</Button>
+		</div>
+	</Modal>
 {/if}
 
-
+{#if error_animal}
+	<Modal bind:open={error_animal} size="xs" autoclose>
+		<div class="text-center">
+			<svg aria-hidden="true" class="mx-auto mb-4 w-14 h-14 text-gray-400 dark:text-gray-200" fill="none" stroke="red" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+			<h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Non hai ancora inserito animali</h3>
+			<Button color='alternative' href="#/profile/pets">Clicca qui</Button>
+		</div>
+	</Modal>
+{/if}
 
 <div id="prenotazione" style="display:none;">   <!--  DA CAMBIAREEEEEE -->
 	<p class="mt-5 ml-10 font-serif xl:text-2xl sm:text-lg text-gray-900 dark:text-white">Ecco le sedi che offrono il servizio</p>
@@ -272,6 +279,7 @@
 				{/each}
 			</select>
 		</div>
+		
 		<Accordion multiple class="mt-5 mr-5">
 			<!-- SELEZIONE SOLO PER SEDE, OPPURE PER SEDE E SERVIZIO -->
 			{#if selected_sits.length != 0}
