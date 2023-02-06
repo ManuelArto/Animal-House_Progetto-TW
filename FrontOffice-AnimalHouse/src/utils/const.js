9ad8a584e8696271497fce61de4d63f90fad67b1
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-02-06 17:34:54 +0100

    Leaderboard conclusa - aggiunto link al game

--- FrontOffice-AnimalHouse/src/utils/const.js
+++ FrontOffice-AnimalHouse/src/utils/const.js
@@ -1,6 +1,8 @@
+const GAME_URL = ""
 const SERVER_URL = "http://localhost:5000"
 
 export const ENDPOINT = {
+	// SERVER
 	LOGIN: SERVER_URL + "/user/login",
 	REGISTER: SERVER_URL + "/user/register",
 	EDIT_DATA: SERVER_URL + "/user/update",
@@ -9,5 +11,7 @@ export const ENDPOINT = {
 	ANIMALS_NEW: SERVER_URL + "/animals/",
 	ANIMALS_DELETE: (animalId) => SERVER_URL + `/animals/${animalId}`,
 	SCORES_LIST: SERVER_URL + "/scores/list",
-	THREADS_LIST: SERVER_URL + "/threads/list"
+	THREADS_LIST: SERVER_URL + "/threads/list",
+	// GAME
+	GAME_QUIZ: GAME_URL + "/games/quiz"
 }
