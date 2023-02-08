31e80ace0b0e5cca76b2b168d8e91e8afd9d48bb
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-02-08 01:42:20 +0100

    Aggiunto else se non ci sono messaggi per quel thread

--- FrontOffice-AnimalHouse/src/views/ServiziComunità/BachecaEccoloQua.svelte
+++ FrontOffice-AnimalHouse/src/views/ServiziComunità/BachecaEccoloQua.svelte
@@ -145,6 +145,16 @@
                         </p>
                     </div>
                 </div>
+                {:else}
+                    <div class="bg-white shadow p-8 text-center mx-auto my-10 w-1/3 rounded-3xl">
+                        <h2 class="text-xl font-bold text-gray-800 mb-4">Ancora nessun messaggio</h2>
+                        <Button class="bg-blue-500 mt-4" on:click={() => !isUserLogged ? showUnauthorizedAlert("postare un nuovo messaggio") : isNewPostModalOpen = true }>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                                <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"/>
+                            </svg>
+                            <span class="ml-2">Scrivi per primo</span>
+                        </Button>
+                    </div>
                 {/each}
             </div>
         </div>
