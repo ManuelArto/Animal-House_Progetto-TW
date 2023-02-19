b51ab76f6b8ef2c4961d28c673a749e80aa30130
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-02-19 16:27:45 +0100

    sistemato prodotti

--- BackOffice-AnimalHouse/src/components/shop/prodotti/prodotti.ts
+++ BackOffice-AnimalHouse/src/components/shop/prodotti/prodotti.ts
@@ -3,20 +3,18 @@ import products_html from "./prodotti.html?raw"
 
 export function setupProducts(element: JQuery<HTMLDivElement>) {
 	element.html(products_html)
-	$(document).ready(function() {
+	$(function() {
 		// Selezioniamo l'elemento con id filterDropdown e aggiungiamo un listener all'evento click
-		$('#filterDropdown input[type=checkbox]').click(function() {
+		$('#filterDropdown input[type=checkbox]').on("click", function() {
 		  // Definiamo un array vuoto per contenere le categorie selezionate
-		  let selectedCategories = [];
-	  
+		  let selectedCategories: string[] = [];
 		  // Selezioniamo tutti gli elementi input con tipo checkbox all'interno dell'elemento con id filterDropdown
 		  $('#filterDropdown input[type=checkbox]').each(function() {
 			// Se l'elemento è selezionato, aggiungiamo il valore dell'attributo id all'array selectedCategories
 			if ($(this).is(':checked')) {
-			  selectedCategories.push($(this).attr('id'));
+			  selectedCategories.push($(this).attr('id')!);
 			}
 		  });
-	  
 		  // Se non è stata selezionata nessuna categoria, mostriamo tutti i prodotti
 		  if (selectedCategories.length == 0) {
 			$('tbody tr').show();
