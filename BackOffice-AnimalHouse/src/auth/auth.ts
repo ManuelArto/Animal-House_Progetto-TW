6209ae98ec2d2cb0ea51eaf88081e85879b500ab
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-02-20 22:01:04 +0100

    Aggiunto controllo isUserAuthenticated

--- BackOffice-AnimalHouse/src/auth/auth.ts
+++ BackOffice-AnimalHouse/src/auth/auth.ts
@@ -1,4 +1,43 @@
+import { ENDPOINT } from "../utils/const";
+
 export function isUserAuthenticated() {
-	// TODO: copia la gestione dell'autenticazione da user.json del frontoffice
+	let token: string | null
+	if (token = localStorage.getItem("token")) {
+		if (isTokenExpired(token)) {
+			localStorage.clear()
+			return false
+		} else {
+			setRefreshTokenTimer(token)
+			return true
+		}
+	}
+
+	// TODO: cambia a false 
 	return true
+}
+
+function isTokenExpired(token: string) {
+	let tokens = token.split(".");
+	let exp = JSON.parse(window.atob(tokens[1])).exp * 1000 // in millisecond
+
+	return exp <= +new Date()
+}
+
+function setRefreshTokenTimer(token: string) {
+	let tokens = token.split(".");
+	let exp = JSON.parse(window.atob(tokens[1])).exp * 1000 // in millisecond
+	let timer = exp - +new Date()
+	
+	// set interval for refreshing the token
+	setTimeout(async () => await refreshToken(token), timer - 30000) // 30 seconds before expired
+}
+
+async function refreshToken (token: string) {
+	const response = await fetch(ENDPOINT.REFRESH_TOKEN, {
+		headers: { 'Authorization': `Bearer ${token}`}
+	})
+
+	const data = await response.json()
+	if (!data.error)
+		localStorage.setItem("token", data.token)
 }
