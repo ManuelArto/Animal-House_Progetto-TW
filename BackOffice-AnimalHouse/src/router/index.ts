829545c05ad90e068b49ac80deb9ac4bf37ca79e
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-02-20 16:59:32 +0100

    commit bello

--- BackOffice-AnimalHouse/src/router/index.ts
+++ BackOffice-AnimalHouse/src/router/index.ts
@@ -2,16 +2,19 @@ import $ from "jquery"
 import { setupProducts } from '../components/shop/prodotti/prodotti'
 import { setupUsers } from '../components/anagrafica/user/user'
 import { setupScore } from '../components/anagrafica/score/score'
+import { setupBacheca } from "../components/comunita/bacheca"
 import { setupNotFound } from "../components/NotFound/notfound"
 
 const routes: { [key: string]: Function } = {
 	"#/shop/prodotti": setupProducts,
 	"#/anagrafica/utenti": setupUsers,
 	"#/anagrafica/punteggi": setupScore,
+	"#/comunita/bacheca": setupBacheca,
 	"notFound": setupNotFound
 }
 
 export function render(path: string) {
+	console.log(path);
 	if (routes[path])
 		routes[path]($("#main"))
 	else 
