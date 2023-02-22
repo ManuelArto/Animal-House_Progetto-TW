ba7b478862d0c30177af58a901a9e36af9e927e4
Andrea Napoli <andrea.napoli4@studio.unibo.it>
2023-02-22 18:03:44 +0100

    bacheca sistemata

--- BackOffice-AnimalHouse/src/components/comunita/bacheca.ts
+++ BackOffice-AnimalHouse/src/components/comunita/bacheca.ts
@@ -30,13 +30,18 @@ function initThreads(deleteModal: Modal) {
 			product_tmpl.find("#title").attr("onClick", function() {
 				table.hide();
 				mex.show();
-				initMessages(deleteModal, thread.title, thread.messages);
+				$("#back").on("click", function() {
+					mex.hide();
+					table.show();
+				})
+				$("#title_thread").text(thread.title)
+				initMessages(deleteModal, thread.messages);
 			})
 			product_tmpl.find("#creatorUsername").text(thread.creatorUsername)
 			product_tmpl.find("#createDate").text(thread.createdAt.substring(0,10))
 			product_tmpl.find("#createHour").text(thread.createdAt.substring(11,19))
 			
-			$("tbody").append(product_tmpl[0].outerHTML);
+			$("threads_table").append(product_tmpl[0].outerHTML);
 
 			// $(`#${product._id}`).data("id", product._id)
 			$(`#delete_${thread._id}`).on("click", () => deleteModal.toggle())
@@ -47,22 +52,18 @@ function initDeleteModal(deleteModal: Modal) {
 	$(".closeDeleteProductModal").on("click", () => deleteModal.hide() )
 }
 
-function initMessages(deleteModal: Modal, title: string, messages: Message[]){
+function initMessages(deleteModal: Modal, messages: Message[]){
 	var product_tmpl = $($("#message_template").html());
-	product_tmpl.find("#title").text(title)
 
 	messages.forEach((message:Message) => {
 		product_tmpl.find(".deleteMessageButton").attr("id", `delete_${message._id}`)
-		product_tmpl.find("#back").attr("onClick", function() {
-			mex.hide();
-			table.show();
-		})
+		
 		product_tmpl.find("#content").text(message.content)
 		product_tmpl.find("#user").text(message.authorUsername)
 		product_tmpl.find("#date").text(message.date)
 		product_tmpl.find("#time").text(message.time)
 		
-		$("tbody").append(product_tmpl[0].outerHTML);
+		$("#messages_table").append(product_tmpl[0].outerHTML);
 
 		// $(`#${product._id}`).data("id", product._id)
 		$(`#delete_${message._id}`).on("click", () => deleteModal.toggle())
