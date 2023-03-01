8ac0fdcdb4148e3180db595f2e95b3822f1a20e4
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-03-01 18:18:15 +0100

    rinominato store animals game

--- Game-AnimalHouse/src/store.ts
+++ Game-AnimalHouse/src/store.ts
@@ -9,16 +9,16 @@ export const state = reactive({
 
 export function addAnimal(animal: Animal) {
 	state.animals.push(animal)
-	localStorage.setItem('animals', JSON.stringify(state.animals))
+	localStorage.setItem('game_animals', JSON.stringify(state.animals))
 }
 export function removeAnimal(id: string) {
 	state.animals = state.animals.filter(animal => animal._id !== id)
-	localStorage.setItem('animals', JSON.stringify(state.animals))
+	localStorage.setItem('game_animals', JSON.stringify(state.animals))
 }
 
 
 // Private methods
 
 function _loadAnimals(): Animal[] {
-	return JSON.parse(localStorage.getItem('animals') || '[]')
+	return JSON.parse(localStorage.getItem('game_animals') || '[]')
 }
